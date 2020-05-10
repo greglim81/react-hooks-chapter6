0bff7656358138a10513266fd254c37aebe79c57
Greg Lim <limjunqu@gmail.com>
2020-05-10 09:22:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import JumbotronComponent from './JumbotronComponent';
+import UserForm from './UserForm';
 
 function App() {
   return (
     <div>
-        <JumbotronComponent>        
-          This is a long sentence, and I want to insert content into the 
-          jumbotron component from the outside.
-        </JumbotronComponent>   
-
+        <UserForm />
     </div>
   );
 }
